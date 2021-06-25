c79242847b4f283b3c4e26ce90c97861a602b766
Phoenix Feder <lo.verba.pe.le.munje@gmail.com>
2021-06-25 20:45:26 +0800

    Tune whitespaces

--- asset/mathjax.js
+++ asset/mathjax.js
@@ -1,6 +1,13 @@
 window.MathJax = {
   options: {
-    processHtmlClass: "math",
+    processHtmlClass: [
+      "math-letter", "math-sign", "math-plain",
+      "math-bracket-round", "math-bracket-square", "math-bracket-curly",
+      "math-bracket-angle", "math-bracket-line",
+      "math-pair", "math-triplet", "math-tuple",
+      "math-serif", "math-sans", "math-mono",
+      "math"
+    ],
     ignoreHtmlClass: "body"
   },
   loader: {
