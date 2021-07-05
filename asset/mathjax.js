26a00451daba7d85682b58aa0a99c51126b80896
Phoenix Feder <lo.verba.pe.le.munje@gmail.com>
2021-07-06 04:14:23 +0800

    Only write catalogs in catalog pages

--- asset/mathjax.js
+++ asset/mathjax.js
@@ -1,28 +1,55 @@
-window.MathJax = {
-  jax: {
-    load: ["input/tex", "output/chtml"]
-  },
-  tex2jax: {
-    packages: {
-      '[+]': ['ams'],
-      '[+]': ['unicode'],
-      '[+]': ['color']
-    },
-    inlineMath: ['\\(', '\\)'],
-    displayMath: ['\\[', '\\]']
-  },
-  SVG: {
-    scale: 1.2,
-    font: "STIX-Web"
-  },
-  options: {
-    processHtmlClass: [
-      "math-letter", "math-sign", "math-plain",
-      "math-bracket-round", "math-bracket-square", "math-bracket-curly",
-      "math-bracket-angle", "math-bracket-line",
-      "math-pair", "math-triplet", "math-tuple",
-      "math-serif", "math-sans", "math-mono",
-    ],
-    ignoreHtmlClass: "body"
+MathJax = {
+   loader: {
+      load: ['input/tex', 'output/svg']
+   },
+   tex: {
+      inlineMath: [['\\(', '\\)']],
+      displayMath: [['\\[', '\\]']],
+      packages: {
+         '[+]': ['ams'],
+         '[+]': ['unicode'],
+         '[+]': ['color']
+      },
+   },
+   svg: {
+      fontCache: 'local',
+      scale: 1
+   },
+   options: {
+      processHtmlClass: [
+         "math-letter", "math-sign", "math-plain",
+         "math-bracket-round", "math-bracket-square", "math-bracket-curly",
+         "math-bracket-angle", "math-bracket-line",
+         "math-pair", "math-triplet", "math-tuple",
+         "math-serif", "math-sans", "math-mono",
+      ],
+      ignoreHtmlClass: ["body"]
+   }
+};
+
+/*
+MathJax = {
+   loader: {
+      load: ['input/tex', 'output/svg']
+   },
+   tex: {
+      packages: {
+         '[+]': ['ams'],
+         '[+]': ['unicode'],
+         '[+]': ['color']
+      },
+      inlineMath: ['\\(', '\\)'],
+      displayMath: ['\\[', '\\]']
+   },
+   options: {
+      processHtmlClass: [
+         "math-letter", "math-sign", "math-plain",
+         "math-bracket-round", "math-bracket-square", "math-bracket-curly",
+         "math-bracket-angle", "math-bracket-line",
+         "math-pair", "math-triplet", "math-tuple",
+         "math-serif", "math-sans", "math-mono",
+      ],
+      ignoreHtmlClass: "body"
   }
 };
+*/
