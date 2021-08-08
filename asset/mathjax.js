9a3d8d5df5ac235144e779b536ecd36b0028b488
Phoenix Feder <lo.verba.pe.le.munje@gmail.com>
2021-08-08 20:41:03 +0800

    Meta: Color Mathjax

--- asset/mathjax.js
+++ asset/mathjax.js
@@ -1,6 +1,6 @@
-MathJax = {
+window.MathJax = {
    loader: {
-      load: ['input/tex', 'output/chtml']
+      load: ['input/tex', 'output/chtml'],
    },
    tex: {
       inlineMath: [['\\(', '\\)']],
@@ -8,11 +8,11 @@ MathJax = {
       packages: {
          '[+]': ['ams'],
          '[+]': ['unicode'],
-         '[+]': ['color']
+         '[+]': ['color'],
       },
    },
    chtml: {
-      scale: 0.9
+      scale: 0.92,
    },
    options: {
       processHtmlClass: [
@@ -20,35 +20,8 @@ MathJax = {
          "math-bracket-round", "math-bracket-square", "math-bracket-curly",
          "math-bracket-angle", "math-bracket-line",
          "math-pair", "math-triplet", "math-tuple",
-         "math-serif", "math-sans", "math-mono", "math-void"
+         "math-serif", "math-sans", "math-mono", "math-void",
       ],
-      ignoreHtmlClass: ["body"]
-   }
-};
-
-/*
-MathJax = {
-   loader: {
-      load: ['input/tex', 'output/svg']
+      ignoreHtmlClass: ["body"],
    },
-   tex: {
-      packages: {
-         '[+]': ['ams'],
-         '[+]': ['unicode'],
-         '[+]': ['color']
-      },
-      inlineMath: ['\\(', '\\)'],
-      displayMath: ['\\[', '\\]']
-   },
-   options: {
-      processHtmlClass: [
-         "math-letter", "math-sign", "math-plain",
-         "math-bracket-round", "math-bracket-square", "math-bracket-curly",
-         "math-bracket-angle", "math-bracket-line",
-         "math-pair", "math-triplet", "math-tuple",
-         "math-serif", "math-sans", "math-mono",
-      ],
-      ignoreHtmlClass: "body"
-  }
 };
-*/
